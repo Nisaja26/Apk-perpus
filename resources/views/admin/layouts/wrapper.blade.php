0f9f03c65e16b02ffdce817526087c9f7fa10c8e
Nisaja26 <nissaikaracmawati@gmail.com>
2024-10-06 22:43:19 +0700

    templateting 02 80%

---
+++ resources/views/admin/layouts/wrapper.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('admin.layouts.head')
+</head>
+<body>
+    @include('admin.layouts.header')
+    @include('admin.layouts.sidebar')
+    
+    <div class="content">
+        @include('admin.layouts.content')
+    </div>
+
+    @include('admin.layouts.footer')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route untuk 'template' harus seperti ini
+Route::get('template', function() {
+    return view('template');
+});
+
+// Route untuk '/'
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.layouts.wrapper');
 });
